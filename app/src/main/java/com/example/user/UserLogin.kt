16b9053102dcde81fb9e954e0e28b8052bbe3f45
NAKAJIMA <230348e@ugs.kochi-tech.ac.jp>
2022-01-10 14:56:42 +0900

    UseLoginとWsClient追加、またbuild.gradleの最後に2行追加

--- app/src/main/java/com/example/user/UserLogin.kt
+++ app/src/main/java/com/example/user/UserLogin.kt
@@ -1,9 +1,23 @@
 package com.example.user
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundlex
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.net.URI
+import org.json.*
 
 class UserLogin : AppCompatActivity() {
+    companion object{
+        const val loginReId: Int = 1
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
