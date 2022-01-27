2bfd815306ef14a6440474ee7e0cabb5ab4579f5
Nikkei225Futures <mt106fd3s@gmail.com>
2022-01-27 14:04:05 +0900

    implement screen to show evaluation detail that user has

---
+++ app/src/main/java/com/example/user/UserEditEvaluation.kt
@@ -0,0 +1,11 @@
+package com.example.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserEditEvaluation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_edit_evaluation)
+    }
+}
